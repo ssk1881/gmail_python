56b72a29b8af038d68f621630996168caa46d568
mysterion <sreekrishna1881@gmail.com>
2017-07-20 17:23:44 +0530

    first commit

---
+++ gmail_py.py
@@ -0,0 +1,36 @@
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
+
+
+
+
+#main program
+userid="MAIL_USERID"
+password="PASSWORD"
+recp=["SOMEONE@gmail.com "]
+subject="hi please open me"
+body="BODY OF THE MAIL"
+for i in recp:
+    while(1):
+        send_email(userid,password,recp,subject,body)
